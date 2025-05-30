254030853bdd55f6fcd1ab6f1f359ab78262ec0b
maoyln <maoyl_yx@163.com>
2025-05-30 14:42:55 +0800

    vue基础属性-模版引用

--- src/views/vueBase/10TemplateRefs/10TemplateRefs.vue
+++ src/views/vueBase/10TemplateRefs/10TemplateRefs.vue
@@ -13,15 +13,53 @@
 
       <div>
         <p>Message is: {{ text }}</p>
-        <input ref="my-input" v-model="text" />
+        <div>
+          3.5之前：【const myInput = ref(null);】必须和模板里的 ref 同名
+        </div>
         <input ref="myInput" v-model="text" />
+        <div>
+          3.5之后：【const inputRef = useTemplateRef('my-input');】
+        </div>
+        <input ref="my-input" v-model="text" />
       </div>
+    </div>
+
+    <hr>
+
+    <div>
+      <h2>组件上的 ref</h2>
+      <div>
+        <Child ref="myChild" />
+
+      </div>
+      <div>
+        有一个例外的情况，使用了 （script setup） 的组件是默认私有的：一个父组件无法访问到一个使用了 （script setup） 的子组件中的任何东西，除非子组件在其中通过 defineExpose 宏显式暴露：
+      </div>
+
+    </div>
+
+    <div>
+      <h2>v-for 中的模板引用</h2>
+      <div>
+        <ul>
+          <li v-for="item in list" ref="items" :key="item">
+            {{ item }}
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <hr>
+    <div>
+      <h2>函数模板引用</h2>
+      <input :ref="(el) => { console.log(el); /* 将 el 赋值给一个数据属性或 ref 变量 */ }">
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, useTemplateRef, onMounted /** watchEffect **/ } from "vue";
+import { ref, useTemplateRef, onMounted, reactive /** watchEffect **/ } from "vue";
+import Child from "./components/Child.vue";
 
 const text = ref("");
 
@@ -50,6 +88,11 @@ const myInput = ref(null);
 //   }
 // })
 
+// 3.5 之前的用法
+const myChild = ref(null);
+
+const myChildRef = useTemplateRef("myChild");
+
 onMounted(() => {
   console.log(inputRef, "inputRef");
   console.log(inputRef.value, "inputRef.value");
@@ -58,5 +101,33 @@ onMounted(() => {
   console.log(myInput.value, "myInput.value");
 
   (inputRef.value as HTMLInputElement).focus();
+
+  console.log('----------------------黄金分割线--------------------------');
+  console.log(myChild, "myChild");
+  console.log(myChild.value, "myChild.value");
+
+  console.log(myChildRef, "myChildRef");
+  console.log(myChildRef.value, "myChildRef.value");
+
+  // 获取字组件的数据
+  const myChildRefData = reactive(myChildRef.value as any);
+  console.log(myChildRefData, "myChildRefData");
+  console.log(myChildRefData.a, "myChildRefData.a");
+  console.log(myChildRefData.b, "myChildRefData.b");
+  console.log((myChildRef.value as any).a, "(myChildRef.value as any).a");
+  console.log((myChildRef.value as any).b, "(myChildRef.value as any).b");
+
+
+  console.log('----------------------黄金分割线--------------------------');
+
+
+  console.log(itemRefs, 'itemRefs');
+  console.log((itemRefs as any).value.map((i: any) => i.textContent), 'itemRefs.value.map(i => i.textContent)');
+  console.log(itemRefs.value, 'itemRefs.value');
 });
+
+const list = ref([1, 2, 3])
+
+// const items = ref([]) // 3.5 前
+const itemRefs = useTemplateRef('items') // 3.5 之后
 </script>
---
+++ src/views/vueBase/10TemplateRefs/components/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    这是一个字组件
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const a = 1
+const b = ref(2)
+
+// 像 defineExpose 这样的编译器宏不需要导入
+// 请注意，defineExpose 必须在任何 await 操作之前调用。否则，在 await 操作后暴露的属性和方法将无法访问。
+defineExpose({
+  a,
+  b
+})
+</script>
