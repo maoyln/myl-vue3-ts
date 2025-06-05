b5d5ba48d13e0a83f4cc7e17c021b96de1d69b51
maoyln <maoyl_yx@163.com>
2025-06-05 11:19:36 +0800

    vue深入组件-插槽Slots-传入多个元素

--- src/views/DepthComponents/06Slots/06Slots.vue
+++ src/views/DepthComponents/06Slots/06Slots.vue
@@ -13,13 +13,42 @@
   <div>
     <h2>插槽内容与出口</h2>
     <div>
+      <div>下面是插槽默认值</div>
       <Child01 />
     </div>
+    <br>
+    <div>
+      <div>下面是插传入值</div>
+      <Child01><span style="color: red; font-size: 20px; border: 1px solid red">这是传入插槽内容</span></Child01>
+    </div>
   </div>
+
+  <hr>
+
+  <div>
+    <h2>传入一个函数</h2>
+    <div>
+      <div>下面是插槽默认值</div>
+      <Child02>
+        Click me
+       </Child02>
+      <Child02>
+        <span style="color:cyan">Click me! </span>
+        <AwesomeIcon />
+      </Child02>
+    </div>
+    <br>
+
+  </div>
+
+  <hr>
 </div>
 </template>
 
 <script setup>
 import Child01 from './components/Child01.vue';
+
+import Child02 from './components/Child02.vue'
+import AwesomeIcon from './components/AwesomeIcon.vue'
 
 </script>
