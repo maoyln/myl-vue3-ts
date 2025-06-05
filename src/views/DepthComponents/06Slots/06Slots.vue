770d623c70227a3dae4d9722366994d0b69f190e
maoyln <maoyl_yx@163.com>
2025-06-05 11:32:30 +0800

    vue深入组件-插槽Slots-具名插槽

--- src/views/DepthComponents/06Slots/06Slots.vue
+++ src/views/DepthComponents/06Slots/06Slots.vue
@@ -22,11 +22,10 @@
       <Child01><span style="color: red; font-size: 20px; border: 1px solid red">这是传入插槽内容</span></Child01>
     </div>
   </div>
-
   <hr>
 
   <div>
-    <h2>传入一个函数</h2>
+    <h2>传入多个元素</h2>
     <div>
       <div>下面是插槽默认值</div>
       <Child02>
@@ -38,9 +37,66 @@
       </Child02>
     </div>
     <br>
-
   </div>
+  <hr>
 
+  <div>
+    <h2>默认内容</h2>
+    <div>
+      <div>没有有slot默认值-则如下所示</div>
+      <Child02>
+        Click me
+       </Child02>
+      <Child02>
+        <span style="color:cyan">Click me! </span>
+        <AwesomeIcon />
+      </Child02>
+    </div>
+    <br>
+    <div>
+      <div>有slot默认值-则如下所示</div>
+      <Child03/>
+
+    </div>
+  </div>
+  <hr>
+  
+  <div>
+    <h2>具名插槽</h2>
+    <Child04>
+      <template v-slot:header>
+        <!-- header 插槽的内容放这里 -->
+         这是一个头部内容
+      </template>
+
+      <template v-slot:footer>
+        <!-- header 插槽的内容放这里 -->
+         这是一个尾部内容
+      </template>
+    </Child04>
+  </div>
+  <hr>
+
+  <div>
+    <h2>具名插槽-简写</h2>
+    <Child04>
+      <template #header>
+        <h1>这可能是一个页面标题</h1>
+      </template>
+    
+      <template #default>
+        <span style="color: red">
+          这是默人值
+        </span>
+        <p>主要内容的一段。</p>
+        <p>还有另一个。</p>
+      </template>
+    
+      <template #footer>
+        <p>这里有一些尾部没人</p>
+      </template>
+    </Child04>
+  </div>
   <hr>
 </div>
 </template>
@@ -49,6 +105,8 @@
 import Child01 from './components/Child01.vue';
 
 import Child02 from './components/Child02.vue'
+import Child03 from './components/Child03.vue'
+import Child04 from './components/Child04.vue'
 import AwesomeIcon from './components/AwesomeIcon.vue'
 
 </script>
---
+++ src/views/DepthComponents/06Slots/components/Child03.vue
@@ -0,0 +1,19 @@
+<template>
+  <button type="submit" class="fancy-btn">
+    <slot>
+      插槽默认值
+    </slot>
+	</button>
+</template>
+
+<style>
+.fancy-btn {
+  color: #fff;
+  background: linear-gradient(315deg, #424cd3 25%, #fcff64);
+  border: none;
+  padding: 5px 10px;
+  margin: 5px;
+  border-radius: 8px;
+  cursor: pointer;
+}
+</style>
