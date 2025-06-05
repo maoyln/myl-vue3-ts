501e0216d6df57744b3bc7d922e828d23527d727
maoyln <maoyl_yx@163.com>
2025-06-05 10:01:52 +0800

    vue基础属性-组件v-model

--- src/views/DepthComponents/04VModel/04VModel.vue
+++ src/views/DepthComponents/04VModel/04VModel.vue
@@ -1,29 +1,70 @@
 <template>
-<div>
-  <h1>
-    <a
-      href="https://cn.vuejs.org/guide/components/v-model.html"
-      target="_blank"
-      >组件 v-models
-    </a>
-  </h1>
-
   <div>
-    <h2>基本用法</h2>
-    <div>v-model 可以在组件上使用以实现双向绑定</div>
+    <h1>
+      <a
+        href="https://cn.vuejs.org/guide/components/v-model.html"
+        target="_blank"
+        >组件 v-models
+      </a>
+    </h1>
+
     <div>
-      从 Vue 3.4 开始，推荐的实现方式是使用 defineModel() 宏：
+      <h2>基本用法</h2>
+      <div>v-model 可以在组件上使用以实现双向绑定</div>
+      <div>从 Vue 3.4 开始，推荐的实现方式是使用 defineModel() 宏：</div>
+      <br />
+      <div>
+        <div>
+          defineModel() 返回的值是一个 ref。它可以像其他 ref
+          一样被访问以及修改，不过它能起到在父组件和当前变量之间的双向绑定的作用：
+        </div>
+
+        <ul>
+          <li>它的 .value 和父组件的 v-model 的值同步；</li>
+          <li>当它被子组件变更了，会触发父组件绑定的值一起更新。</li>
+        </ul>
+        <br />
+        <div>
+          这意味着你也可以用 v-model 把这个 ref 绑定到一个原生 input
+          元素上，在提供相同的 v-model 用法的同时轻松包装原生 input 元素：
+        </div>
+      </div>
+      <br />
+      <div>
+        <div>孩子组件如下：</div>
+        <Child01 v-model="model"></Child01>
+      </div>
     </div>
+
     <div>
-      <Child01 v-model="model"></Child01>
+      <h2>底层机制</h2>
+      <div>defineModel 是一个便利宏。编译器将其展开为以下内容：</div>
+      <ul>
+        <li>一个名为 modelValue 的 prop，本地 ref 的值与其同步；</li>
+        <li>
+          一个名为 update:modelValue 的事件，当本地 ref 的值发生变更时触发。
+        </li>
+      </ul>
+      <br />
+      <div>
+        <div>孩子组件如下：</div>
+        <div>输入值：{{foo}}</div>
+        <Child02
+          :modelValue="foo"
+          @update:modelValue="($event) => (foo = $event)"
+        />
+      </div>
     </div>
+
+    <hr />
   </div>
-</div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import Child01 from './components/Child01.vue';
+<script setup>
+import { ref } from "vue";
+import Child01 from "./components/Child01.vue";
+import Child02 from "./components/Child02.vue";
 
 const model = ref(0);
+const foo = ref('');
 </script>
--- src/views/DepthComponents/04VModel/components/Child01.vue
+++ src/views/DepthComponents/04VModel/components/Child01.vue
@@ -1,13 +1,20 @@
 <!-- Child01.vue -->
 <script setup>
-  const model = defineModel();
 
-  function update() {
-    model.value++;
-  }
+// 注：使 v-model 必填
+// const model = defineModel({ required: true })
+
+// 注：提供一个默认值
+// const model = defineModel({ default: 0 })
+
+const model = defineModel();
+
+function update() {
+  model.value++;
+}
 </script>
 
 <template>
-<div>父绑定v-model是：{{model}}</div>
-<button @click="update">加1</button>
+  <div>父绑定v-model是：{{ model }}</div>
+  <button @click="update">加1</button>
 </template>
