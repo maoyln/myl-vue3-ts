9ea8000098d56119cd423f3db147d3dd52f40c53
maoyln <maoyl_yx@163.com>
2025-06-06 17:59:41 +0800

    vue内置组件- KeepAlive的基本使用

--- src/views/BuiltInComponents/03KeepAlive/03KeepAlive.vue
+++ src/views/BuiltInComponents/03KeepAlive/03KeepAlive.vue
@@ -1,4 +1,3 @@
-
 <template>
   <div>
     <h1>
@@ -13,21 +12,30 @@
         <h2>进入 / 离开动画</h2>
         <div><Child01 /></div>
       </div>
-      <hr>
+      <hr />
 
       <div>
-        <h2>渐进延迟列表动画</h2>
-        <div><Child02 /></div>
-        
+        <h2>包含/排除</h2>
+        <div>include="a,b"</div>
+        <div>:include="/a|b/"</div>
+        <div>:include="['a', 'b']"</div>
       </div>
 
+      <hr />
+      <div>
+        <h2>最大缓存实例数</h2>
+        <div>KeepAlive :max="10"</div>
+      </div>
+
+      <hr />
+      <div>
+        <h2>缓存实例的生命周期​</h2>
+        <div>import { onActivated, onDeactivated } from 'vue'</div>
+      </div>
     </div>
   </div>
-  </template>
-  
-  <script setup>
-  import Child01 from './components/Child01.vue';
-  import Child02 from './components/Child02.vue';
+</template>
 
-
-  </script>
+<script setup>
+import Child01 from "./components/Child01.vue";
+</script>
