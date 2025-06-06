bc727fc8cd01f7b0d663cf9114c96b7afc4c16ee
maoyln <maoyl_yx@163.com>
2025-06-06 11:15:32 +0800

    vue深入组- 组合式函数- mouse组合式函数

--- src/views/LogicReuse/01Composables/mouse.js
+++ src/views/LogicReuse/01Composables/mouse.js
@@ -1,5 +1,6 @@
 // mouse.js
 import { ref, onMounted, onUnmounted } from 'vue'
+import { useEventListener } from './event';
 
 // 按照惯例，组合式函数名以“use”开头
 export function useMouse() {
@@ -7,16 +8,21 @@ export function useMouse() {
   const x = ref(0)
   const y = ref(0)
 
-  // 组合式函数可以随时更改其状态。
-  function update(event) {
+  // // 组合式函数可以随时更改其状态。
+  // function update(event) {
+  //   x.value = event.pageX
+  //   y.value = event.pageY
+  // }
+
+  // // 一个组合式函数也可以挂靠在所属组件的生命周期上
+  // // 来启动和卸载副作用
+  // onMounted(() => window.addEventListener('mousemove', update))
+  // onUnmounted(() => window.removeEventListener('mousemove', update))
+
+  useEventListener(window, 'mousemove', (event) => {
     x.value = event.pageX
     y.value = event.pageY
-  }
-
-  // 一个组合式函数也可以挂靠在所属组件的生命周期上
-  // 来启动和卸载副作用
-  onMounted(() => window.addEventListener('mousemove', update))
-  onUnmounted(() => window.removeEventListener('mousemove', update))
+  })
 
   // 通过返回值暴露所管理的状态
   return { x, y }
